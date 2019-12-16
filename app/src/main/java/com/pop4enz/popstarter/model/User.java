6fffdc3fdfe88db400b3cd8639bb6cc493b33ea4
Pavel <Pavlik2012konez@gmail.com>
2019-12-16 09:56:30 +0300

    initial

---
+++ app/src/main/java/com/pop4enz/popstarter/model/User.java
@@ -0,0 +1,85 @@
+package com.pop4enz.popstarter.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("first_name")
+    @Expose
+    private String first_name;
+    @SerializedName("last_name")
+    @Expose
+    private String last_name;
+    @SerializedName("picture")
+    @Expose
+    private String picture;
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("email")
+    @Expose
+    private String email;
+
+    public User() {
+    }
+
+    public User(Integer id, String first_name, String last_name, String picture, String username, String email) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.picture = picture;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
